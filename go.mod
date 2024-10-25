93b9e40f7521f08de10c727f11e472861dadbbbc
root <root@LIYL66-PF51YP6V.lenovo.com>
2024-10-25 10:55:09 +0800

    基于sqlmock实现userdao的单元测试

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22
 toolchain go1.22.2
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/alibabacloud-go/darabonba-openapi v0.2.1
 	github.com/alibabacloud-go/dysmsapi-20170525/v2 v2.0.18
 	github.com/aliyun/alibaba-cloud-sdk-go v1.63.23
--- internal/repository/dao/user_test.go
+++ internal/repository/dao/user_test.go
@@ -1,7 +1,103 @@
 package dao
 
-import "testing"
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	gormMySQL "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func TestGORMUserDAO_Insert(t *testing.T) {
-	
+	testcases := []struct {
+		name    string
+		mock    func(t *testing.T) *sql.DB
+		ctx     context.Context
+		user    User
+		wantErr error
+	}{
+		{
+			name: "插入成功",
+			mock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				res := sqlmock.NewResult(1, 1)
+				// 这边预期的是正则表达式
+				// 这个写法的意思就是，只要是 INSERT 到 users 的语句
+				mock.ExpectExec("INSERT INTO `users` .*").
+					WillReturnResult(res)
+				require.NoError(t, err)
+				return mockDB
+			},
+			user: User{
+				Email: sql.NullString{String: "test@example.com", Valid: true},
+			},
+			ctx:     context.Background(),
+			wantErr: nil,
+		},
+		{
+			name: "邮箱冲突 或 手机号码冲突",
+			mock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				// 这边预期的是正则表达式
+				// 这个写法的意思就是，只要是 INSERT 到 users 的语句
+				mock.ExpectExec("INSERT INTO `users` .*").
+					WillReturnError(&mysql.MySQLError{
+						Number: 1062,
+					})
+				require.NoError(t, err)
+				return mockDB
+			},
+			user: User{
+				Email: sql.NullString{String: "test@example.com", Valid: true},
+			},
+			ctx:     context.Background(),
+			wantErr: ErrUserDuplicate,
+		},
+		{
+			name: "数据库崩了",
+			mock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				// 这边预期的是正则表达式
+				// 这个写法的意思就是，只要是 INSERT 到 users 的语句
+				mock.ExpectExec("INSERT INTO `users` .*").
+					WillReturnError(errors.New("db error"))
+				require.NoError(t, err)
+				return mockDB
+			},
+			user: User{
+				Email: sql.NullString{String: "test@example.com", Valid: true},
+			},
+			ctx:     context.Background(),
+			wantErr: errors.New("db error"),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			db, err := gorm.Open(gormMySQL.New(gormMySQL.Config{
+				Conn: tc.mock(t),
+				// SELECT VERSION;
+				SkipInitializeWithVersion: true,
+			}), &gorm.Config{
+				// 你 mock DB 不需要 ping
+				DisableAutomaticPing: true,
+				// 这个是什么呢？
+				SkipDefaultTransaction: true,
+			})
+			require.NoError(t, err)
+
+			// 实例化UserDAO
+			d := NewUserDAO(db)
+			err = d.Insert(tc.ctx, tc.user)
+			assert.Equal(t, tc.wantErr, err)
+		})
+
+	}
+
 }
