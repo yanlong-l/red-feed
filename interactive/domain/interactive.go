97403cf1ce3bc3a242a7da3fcd404981e447f5d0
yanlong-li <1961445898@qq.com>
2025-07-25 18:17:08 +0800

    构建api (未完成)

--- interactive/domain/interactive.go
+++ interactive/domain/interactive.go
@@ -1,10 +1,11 @@
 package domain
 
 type Interactive struct {
-	BizId      int64 `json:"biz_id"`
-	ReadCnt    int64 `json:"read_cnt"`
-	LikeCnt    int64 `json:"like_cnt"`
-	CollectCnt int64 `json:"collect_cnt"`
-	Liked      bool  `json:"liked"`
-	Collected  bool  `json:"collected"`
+	Biz        string `json:"biz"`
+	BizId      int64  `json:"biz_id"`
+	ReadCnt    int64  `json:"read_cnt"`
+	LikeCnt    int64  `json:"like_cnt"`
+	CollectCnt int64  `json:"collect_cnt"`
+	Liked      bool   `json:"liked"`
+	Collected  bool   `json:"collected"`
 }
